49ee72a7ba749dc3f34f510ae7c280f4ddbae08a
Guilherme A. Marabezzi Clerice <marabezzi@yahoo.com.br>
2022-11-09 10:18:53 -0300

    Cadastrando especialidades

---
+++ src/main/java/com/mballem/curso/security/repository/EspecialidadeRepository.java
@@ -0,0 +1,9 @@
+package com.mballem.curso.security.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mballem.curso.security.domain.Especialidade;
+
+public interface EspecialidadeRepository extends JpaRepository<Especialidade, Long>{
+
+}
