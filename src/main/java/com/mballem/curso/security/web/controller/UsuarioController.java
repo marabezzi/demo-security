26579b7c1b9c27e25ee83101faa475bf37fa3a65
Guilherme A. Marabezzi Clerice <marabezzi@yahoo.com.br>
2022-11-09 13:17:05 -0300

    Processo para listagem de usuários cadastrados

--- src/main/java/com/mballem/curso/security/web/controller/UsuarioController.java
+++ src/main/java/com/mballem/curso/security/web/controller/UsuarioController.java
@@ -1,17 +1,42 @@
 package com.mballem.curso.security.web.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.mballem.curso.security.domain.Usuario;
+import com.mballem.curso.security.service.UsuarioService;
+
+
 
 @Controller
 @RequestMapping("u")
 public class UsuarioController {
 
+	@Autowired
+	private UsuarioService service;
+	
 	@GetMapping("/novo/cadastro/usuario")
 	public String cadastroPorAdminMedicoPaciente(Usuario usuario) {
 		return "usuario/cadastro";
 	}
+	
+	// abrir lista de usuarios
+    @GetMapping("/lista")
+    public String listarUsuarios() {
+
+        return "usuario/lista";
+    }  
+
+    // listar usuarios na datatables
+    @GetMapping("/datatables/server/usuarios")
+    public ResponseEntity<?> listarUsuariosDatatables(HttpServletRequest request) {
+        return ResponseEntity.ok(service.buscarTodos(request));
+    } 
+    
+    
 }
