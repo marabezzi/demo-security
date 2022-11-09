ecdcf739c0dbc459d7fc468abf68a92e47642bd2
Guilherme A. Marabezzi Clerice <marabezzi@yahoo.com.br>
2022-11-09 14:07:13 -0300

    Restringindo cadastros de usuários já existentes

--- src/main/java/com/mballem/curso/security/web/controller/UsuarioController.java
+++ src/main/java/com/mballem/curso/security/web/controller/UsuarioController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -44,19 +45,24 @@ public class UsuarioController {
         return ResponseEntity.ok(service.buscarTodos(request));
     } 
     
-    // salvar cadastro de usuarios por 	administrador
+    // salvar cadastro de usuarios por administrador
     @PostMapping("/cadastro/salvar")
     public String salvarUsuarios(Usuario usuario, RedirectAttributes attr) {
     	List<Perfil> perfis = usuario.getPerfis();
-    	if (perfis.size() > 2 ||
-    			perfis.contains(Arrays.asList(new Perfil(1L), new Perfil(3L))) ||
-    			perfis.contains(Arrays.asList(new Perfil(2L), new Perfil(3L)))) {
-    		attr.addAttribute("falha", "Paciente não pode ser ADMIN e/ou MÉDICO.");
-    		attr.addAttribute("usuario", usuario);
+    	if (perfis.size() > 2 || 
+    			perfis.containsAll(Arrays.asList(new Perfil(1L), new Perfil(3L))) ||
+    			perfis.containsAll(Arrays.asList(new Perfil(2L), new Perfil(3L)))) {
+    		attr.addFlashAttribute("falha", "Paciente não pode ser Admin e/ou Médico.");
+    		attr.addFlashAttribute("usuario", usuario);
     	} else {
-    		service.salvarUsuario(usuario);
-    		attr.addFlashAttribute("sucesso","Operação realizada com sucesso!");
+    		try {
+    			service.salvarUsuario(usuario); 
+    			attr.addFlashAttribute("sucesso", "Operação realizada com sucesso!");
+    		} catch (DataIntegrityViolationException ex) {
+    			attr.addFlashAttribute("falha", "Cadastro não realizado, email já existente.");
+			}
     	}
     	return "redirect:/u/novo/cadastro/usuario";
-    	}
+    }
+    
     }
