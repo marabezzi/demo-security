9231afa156458c8afc37cbfeb487c7ac742d86ae
Guilherme A. Marabezzi Clerice <marabezzi@yahoo.com.br>
2022-11-09 09:32:07 -0300

    Primeira regra de autorização por perfil

--- src/main/java/com/mballem/curso/security/config/SecurityConfig.java
+++ src/main/java/com/mballem/curso/security/config/SecurityConfig.java
@@ -22,6 +22,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		.antMatchers("/webjars/**", "/css/**", "/image/**", "/js/**").permitAll()
 		.antMatchers("/","/home").permitAll()
 		
+		// acessos privados admin
+		.antMatchers("/u/**").hasAuthority("ADMIN")
+		
+		// acessos privados medicos
+		.antMatchers("/medicos/**").hasAuthority("MEDICO")
+		
 		.anyRequest().authenticated()
 		.and()
 			.formLogin()
