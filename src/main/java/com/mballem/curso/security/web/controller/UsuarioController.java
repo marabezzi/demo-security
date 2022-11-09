870649968451545f6b8ac9b015bd3077a09dfa6b
Guilherme A. Marabezzi Clerice <marabezzi@yahoo.com.br>
2022-11-09 15:19:44 -0300

    Editando credenciais de usuários

--- src/main/java/com/mballem/curso/security/web/controller/UsuarioController.java
+++ src/main/java/com/mballem/curso/security/web/controller/UsuarioController.java
@@ -10,8 +10,10 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.mballem.curso.security.domain.Perfil;
@@ -65,4 +67,10 @@ public class UsuarioController {
     	return "redirect:/u/novo/cadastro/usuario";
     }
     
+    // pré edicao de credenciais e usuarios
+    @GetMapping("/editar/credenciais/usuario/{id}")
+    public ModelAndView preEditarCredenciais(@PathVariable("id") Long id){
+    
+    	return new ModelAndView("usuario/cadastro", "usuario", service.buscarPorId(id));
     }
+}
