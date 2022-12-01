1b52375a8d89ecc76a2be67fa15a601f76bbb607
Guilherme A. Marabezzi Clerice <marabezzi@yahoo.com.br>
2022-12-01 11:54:06 -0300

    Editando os dados pessoais do paciente

--- src/main/java/com/mballem/curso/security/web/controller/PacienteController.java
+++ src/main/java/com/mballem/curso/security/web/controller/PacienteController.java
@@ -48,4 +48,17 @@ public class PacienteController {
 
 		return "paciente/cadastro";
 	}
+	
+	@PostMapping("/editar")
+	public String editar(Paciente paciente, ModelMap model, @AuthenticationPrincipal User user) {
+		Usuario u = usuarioService.buscarPorEmail(user.getUsername());
+		if (UsuarioService.isSenhaCorreta(paciente.getUsuario().getSenha(), u.getSenha())) {
+			service.editar(paciente);
+			model.addAttribute("sucesso", "Seus dados foram editados com sucesso.");
+		} else {
+			model.addAttribute("falha", "Sua senha não confere, tente novamente.");
+		}
+
+		return "paciente/cadastro";
+	}
 }
