80b3111f39891583ca14564c50266897e745eded
Guilherme A. Marabezzi Clerice <marabezzi@yahoo.com.br>
2022-12-02 11:04:37 -0300

    Selecionando a data e hora da consulta

--- src/main/java/com/mballem/curso/security/web/controller/AgendamentoController.java
+++ src/main/java/com/mballem/curso/security/web/controller/AgendamentoController.java
@@ -1,19 +1,37 @@
 package com.mballem.curso.security.web.controller;
 
+import java.time.LocalDate;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.mballem.curso.security.domain.Agendamento;
+import com.mballem.curso.security.service.AgendamentoService;
 
 @Controller
 @RequestMapping("agendamentos")
 public class AgendamentoController {
 	
+	@Autowired
+	private AgendamentoService service;
+	
 	@GetMapping({"/agendar"})
 	public String agendarConsulta(Agendamento agendamento) {
 		
 		return "agendamento/cadastro";	
+	}
+	
+	@GetMapping("/horario/medico/{id}/data/{data}")
+	public ResponseEntity<?> getHorarios(@PathVariable("id") Long id,
+										 @PathVariable("data") @DateTimeFormat(iso = ISO.DATE) LocalDate data){
+		return ResponseEntity.ok(service.buscarHorariosNaoAgendadosPorMedicoIdEData(id, data));
+				
 	}
 
 }
