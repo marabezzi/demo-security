393439ec94b76cd81e51cce50a96c68d3836ba62
Guilherme A. Marabezzi Clerice <marabezzi@yahoo.com.br>
2022-12-02 14:50:23 -0300

    Projeção de histórico de agendamentos

--- src/main/java/com/mballem/curso/security/service/AgendamentoService.java
+++ src/main/java/com/mballem/curso/security/service/AgendamentoService.java
@@ -2,22 +2,30 @@ package com.mballem.curso.security.service;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.mballem.curso.security.datatables.Datatables;
+import com.mballem.curso.security.datatables.DatatablesColunas;
 import com.mballem.curso.security.domain.Agendamento;
 import com.mballem.curso.security.domain.Horario;
 import com.mballem.curso.security.repository.AgendamentoRepository;
+import com.mballem.curso.security.repository.projection.HistoricoPaciente;
 
 @Service
 public class AgendamentoService {
 
 	@Autowired
 	private AgendamentoRepository repository;
+	
+	@Autowired
+	private Datatables datatables;
 	
 	@Transactional(readOnly = true)
 	public List<Horario> buscarHorariosNaoAgendadosPorMedicoIdEData(Long id, LocalDate data) {
@@ -30,16 +38,20 @@ public class AgendamentoService {
 	}
 
 	@Transactional(readOnly = true)
-	public Object buscarHistoricoPorPacienteEmail(String username, HttpServletRequest request) {
-		// TODO Auto-generated method stub
-		return null;
+	public Map<String, Object> buscarHistoricoPorPacienteEmail(String email, HttpServletRequest request) {
+		datatables.setRequest(request);
+		datatables.setColunas(DatatablesColunas.AGENDAMENTOS);
+		Page<HistoricoPaciente> page = repository.findHistoricoByPacienteEmail(email, datatables.getPageable());
+		return datatables.getResponse(page);
 	}
 
 
 	@Transactional(readOnly = true)
-	public Object buscarHistoricoPorMedicoEmail(String username, HttpServletRequest request) {
-		// TODO Auto-generated method stub
-		return null;
+	public Map<String, Object> buscarHistoricoPorMedicoEmail(String email, HttpServletRequest request) {
+		datatables.setRequest(request);
+		datatables.setColunas(DatatablesColunas.AGENDAMENTOS);
+		Page<HistoricoPaciente> page = repository.findHistoricoPorMedicoEmail(email, datatables.getPageable());
+		return datatables.getResponse(page);
 	}
 
 }
