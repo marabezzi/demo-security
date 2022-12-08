ea7672b55a3800ffc4a2c4da9944212dd884c06b
Guilherme A. Marabezzi Clerice <marabezzi@yahoo.com.br>
2022-12-08 10:45:22 -0300

    Mensagem de erro na tentativa de um segundo login

--- src/main/java/com/mballem/curso/security/web/controller/HomeController.java
+++ src/main/java/com/mballem/curso/security/web/controller/HomeController.java
@@ -1,7 +1,10 @@
 package com.mballem.curso.security.web.controller;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import org.springframework.security.web.authentication.session.SessionAuthenticationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,7 +26,16 @@ public class HomeController {
 
 	// login invalido
 		@GetMapping({"/login-error"})
-		public String loginError(ModelMap model) {
+		public String loginError(ModelMap model, HttpServletRequest resp) {
+			HttpSession session = resp.getSession();
+			String lastException = String.valueOf(session.getAttribute("SPRING_SECURITY_LAST_EXCEPTION"));
+			if (lastException.contains(SessionAuthenticationException.class.getName())) {
+				model.addAttribute("alerta", "erro");
+				model.addAttribute("titulo", "Acesso recusado!");
+				model.addAttribute("texto", "Você já está logado em outro dispositivo.");
+				model.addAttribute("subtexto", "Faça o lagout ou espere sua sessão expirar.");
+				return "login";
+			}
 			model.addAttribute("alerta", "erro");
 			model.addAttribute("titulo", "Credenciais inválidas!");
 			model.addAttribute("texto", "Login ou senha incorretos, tente novamente.");
